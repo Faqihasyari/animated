782e86ad588e5da7e7e289f8cd0d8dcd927f6406
Faqihasyari <faqih8158@gmail.com>
2025-10-12 20:25:42 +0700

    add quiz page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -303,6 +303,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
                 ],
               ),
+              
             ],
           ),
         ),
---
+++ lib/quizpage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class Quizpage extends StatefulWidget {
+  const Quizpage({super.key});
+
+  @override
+  State<Quizpage> createState() => _QuizpageState();
+}
+
+class _QuizpageState extends State<Quizpage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
