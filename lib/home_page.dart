16f042fe98f21f1b67c4fbc4abade56400fc4d31
Faqihasyari <faqih8158@gmail.com>
2025-10-23 22:52:08 +0700

    add fetch quizz data

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -16,6 +16,8 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   String userRank = '';
+  List<int> questionCounts = [];
+
   String userName = '';
   var nameList = ['Football', 'Science', 'Fashion', 'Movie', 'Music'];
   var assetList = [
@@ -28,10 +30,43 @@ class _MyHomePageState extends State<MyHomePage> {
 
   final List<String> nameList2 = ['Quiz Sejarah', 'Geography', 'Technology'];
 
+  Future<void> fetchQuizData() async {
+  try {
+    final response = await http.get(
+      Uri.parse('http://192.168.217.231:8000/api/quizzes'),
+      headers: {'Content-Type': 'application/json'},
+    );
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+
+      // Misal struktur respons seperti:
+      // [
+      //   {"name": "Quiz Sejarah", "questions": [ ... ]},
+      //   {"name": "Geography", "questions": [ ... ]}
+      // ]
+
+      setState(() {
+        questionCounts = List<int>.from(
+          data.map((quiz) => quiz['questions']?.length ?? 0),
+        );
+      });
+
+      print('Jumlah soal per quiz: $questionCounts');
+    } else {
+      print('Gagal fetch quiz: ${response.body}');
+    }
+  } catch (e) {
+    print('Error ambil data: $e');
+  }
+}
+
+
   @override
   void initState() {
     super.initState();
     _loadUserName();
+    fetchQuizData();
   }
 
   Future<void> submitAnswer(bool isCorrect) async {
@@ -352,7 +387,10 @@ class _MyHomePageState extends State<MyHomePage> {
                                 ),
                               ),
                             ),
-                            Text(nameList2[index]),
+                            Text(
+                              nameList2[index],
+                              style: TextStyle(fontWeight: FontWeight.w600),
+                            ),
                           ],
                         ),
                       ),
