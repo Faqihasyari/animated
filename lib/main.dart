3bcf526c337b2e6f25a4d3fadcacdec28aa6c729
Faqihasyari <faqih8158@gmail.com>
2025-10-30 14:33:46 +0700

    add arsitektur getx

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: BottomNavScreen(),
+      home: Signin(),
       debugShowCheckedModeBanner: false,
     );
   }
